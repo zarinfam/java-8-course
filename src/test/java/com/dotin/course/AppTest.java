29758fc14bd32087d0c97ea325ccefd183d2c405
Saeed Zarinfam <zarinfam.s@gmail.com>
2017-12-11 23:15:39 +0330

    create test infra.

--- src/test/java/com/dotin/course/AppTest.java
+++ src/test/java/com/dotin/course/AppTest.java
@@ -3,19 +3,20 @@ package com.dotin.course;
 import com.dotin.course.model.Apple;
 import org.junit.Before;
 import org.junit.Test;
+
 import java.util.List;
 
 public class AppTest {
 
-    private List<Apple> appleList ;
+    private List<Apple> appleList;
 
     @Before
-    public void init(){
+    public void init() {
         appleList = DataUtil.createSampleApple();
     }
 
     @Test
-    public void success_filter_green_apple(){
+    public void success_filter_green_apple() {
 
     }
 
